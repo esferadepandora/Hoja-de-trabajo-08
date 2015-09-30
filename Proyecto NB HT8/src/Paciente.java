29ca465d5742b930c1dae721702f38b3671c3bd6
Fernanda Dávila <fernandadavila@otroround.com>
2015-09-30 07:33:05 -0600

    Con clase Paciente.

---
+++ Proyecto NB HT8/src/Paciente.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author EsferaDePandora
+ */
+public class Paciente {
+    //ATRIBUTOS
+    private String nombre;
+    private String padecimiento;
+    private String prioridad;
+    
+    //MÉTODOS
+}
