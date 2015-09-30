472c7d125abfa7ad3ec21e2e34e4c67036afc605
Fernanda Dávila <fernandadavila@otroround.com>
2015-09-30 07:44:38 -0600

    Con clase Paciente actualizada.

--- Proyecto NB HT8/src/Paciente.java
+++ Proyecto NB HT8/src/Paciente.java
@@ -14,5 +14,38 @@ public class Paciente {
     private String padecimiento;
     private String prioridad;
     
-    //MÉTODOS
+    //CONSTRUCTORES
+    public Paciente(String nombre, String padecimiento, String prioridad) {
+	this.nombre=nombre;
+        this.padecimiento=padecimiento;
+        this.prioridad=prioridad;
+    }
+    public Paciente() {
+	nombre="";
+        padecimiento="";
+        prioridad="";
+    }
+    
+    //GETTERS
+    public String getNombre(){
+        return nombre;
+    }
+    public String getPadecimiento(){
+        return padecimiento;
+    }
+    public String getPrioridad(){
+        return prioridad;
+    }
+    
+    //SETTERS
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+    public void setPadecimiento(String padecimiento){
+        this.padecimiento=padecimiento;
+    }
+    public void setPrioridad(String prioridad){
+        this.prioridad=prioridad;
+    }
+    
 }
